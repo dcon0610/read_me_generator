a7bc5546f0d9eced405134387b855c4873bb75ff
David Connell <davecon88@gmail.com>
2020-10-26 23:05:58 +1030

    test license

--- Develop/index.js
+++ Develop/index.js
@@ -68,10 +68,11 @@ inquirer
 function writeToFile() {
 
 
-  console.log(answers.Title)
+ 
 
     const data=`
 # ${answers.Title}
+[![license]('https://img.shields.io/github/license/DAVFoundation/captain-n3m0.svg?style=flat-square)](https://github.com/DAVFoundation/captain-n3m0/blob/master/LICENSE')
 - [Description](#Description)
 - [Installation Requirements](#Installation-Requirements)
 - [Usage Instructions](#Usage-Instructions)
